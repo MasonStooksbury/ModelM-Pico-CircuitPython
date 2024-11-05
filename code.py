3cb68157df61dbcc8508a90f5c185e1713fd9cc2
Mason Stooksbury <masonstooksbury@gmail.com>
2024-11-04 23:30:06 -0600

    Edits

--- code.py
+++ code.py
@@ -7,99 +7,100 @@ from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
 
-
+# Setup the device as a HID so we can talk to the computer
 kbd = Keyboard(usb_hid.devices)
 layout = KeyboardLayoutUS(kbd)
 
+# Setup the main keymap with column, row, keyname, and keycode
 key_map = {
-    'A': {'S': {'keyname': 'LEFT_CONTROL', 'keycode': '224'},
-          'W': {'keyname': 'RIGHT_CONTROL', 'keycode': '228'}},
-    'B': {'W': {'keyname': 'RIGHT_SHIFT', 'keycode': '229'}},
-    'C': {'Q': {'keyname': 'ESCAPE', 'keycode': '41'},
-          'S': {'keyname': 'GRAVE_ACCENT', 'keycode': '53'},
-          'T': {'keyname': 'ONE', 'keycode': '30'},
-          'U': {'keyname': 'Q', 'keycode': '20'},
-          'V': {'keyname': 'A', 'keycode': '4'},
-          'W': {'keyname': 'Z', 'keycode': '29'}},
-    'D': {'S': {'keyname': 'F1', 'keycode': '58'},
-          'T': {'keyname': 'TWO', 'keycode': '31'},
-          'U': {'keyname': 'W', 'keycode': '26'},
-          'V': {'keyname': 'S', 'keycode': '22'},
-          'W': {'keyname': 'X', 'keycode': '27'}},
-    'E': {'Q': {'keyname': 'F4', 'keycode': '61'},
-          'S': {'keyname': 'F2', 'keycode': '59'},
-          'T': {'keyname': 'THREE', 'keycode': '32'},
-          'U': {'keyname': 'E', 'keycode': '8'},
-          'V': {'keyname': 'D', 'keycode': '7'},
-          'W': {'keyname': 'C', 'keycode': '6'}},
-    'F': {'Q': {'keyname': 'G', 'keycode': '10'},
-          'S': {'keyname': 'FIVE', 'keycode': '34'},
-          'T': {'keyname': 'FOUR', 'keycode': '33'},
-          'U': {'keyname': 'R', 'keycode': '21'},
-          'V': {'keyname': 'F', 'keycode': '9'},
-          'W': {'keyname': 'V', 'keycode': '25'},
-          'X': {'keyname': 'B', 'keycode': '5'}},
-    'G': {'Q': {'keyname': 'F5', 'keycode': '62'},
-          'S': {'keyname': 'F9', 'keycode': '66'},
-          'T': {'keyname': 'F10', 'keycode': '67'},
-          'V': {'keyname': 'BACKSLASH', 'keycode': '49'},
-          'W': {'keyname': 'ENTER', 'keycode': '40'},
-          'X': {'keyname': 'SPACE', 'keycode': '44'}},
-    'H': {'Q': {'keyname': 'H', 'keycode': '11'},
-          'S': {'keyname': 'SIX', 'keycode': '35'},
-          'T': {'keyname': 'SEVEN', 'keycode': '36'},
-          'U': {'keyname': 'U', 'keycode': '24'},
-          'V': {'keyname': 'J', 'keycode': '13'},
-          'W': {'keyname': 'M', 'keycode': '16'},
-          'X': {'keyname': 'N', 'keycode': '17'}},
-    'I': {'Q': {'keyname': 'F6', 'keycode': '63'},
-          'S': {'keyname': 'EQUALS', 'keycode': '46'},
-          'T': {'keyname': 'EIGHT', 'keycode': '37'},
-          'U': {'keyname': 'I', 'keycode': '12'},
-          'V': {'keyname': 'K', 'keycode': '14'},
-          'W': {'keyname': 'COMMA', 'keycode': '54'}},
-    'J': {'S': {'keyname': 'F8', 'keycode': '65'},
-          'T': {'keyname': 'NINE', 'keycode': '38'},
-          'U': {'keyname': 'O', 'keycode': '18'},
-          'V': {'keyname': 'L', 'keycode': '15'},
-          'W': {'keyname': 'PERIOD', 'keycode': '55'}},
-    'K': {'Q': {'keyname': 'QUOTE', 'keycode': '52'},
-          'S': {'keyname': 'MINUS', 'keycode': '45'},
-          'T': {'keyname': 'ZERO', 'keycode': '39'},
-          'U': {'keyname': 'P', 'keycode': '19'},
-          'V': {'keyname': 'SEMICOLON', 'keycode': '51'},
-          'X': {'keyname': 'FORWARD_SLASH', 'keycode': '56'}},
-    'L': {'S': {'keyname': 'DELETE', 'keycode': '76'},
-          'T': {'keyname': 'F11', 'keycode': '68'},
-          'U': {'keyname': 'KEYPAD_SEVEN', 'keycode': '95'},
-          'V': {'keyname': 'KEYPAD_ONE', 'keycode': '89'},
-          'W': {'keyname': 'KEYPAD_NUMLOCK', 'keycode': '83'},
-          'X': {'keyname': 'DOWN_ARROW', 'keycode': '81'}},
-    'M': {'Q': {'keyname': 'KEYPAD_ZERO', 'keycode': '98'},
-          'S': {'keyname': 'INSERT', 'keycode': '73'},
-          'T': {'keyname': 'F12', 'keycode': '69'},
-          'U': {'keyname': 'KEYPAD_EIGHT', 'keycode': '96'},
-          'V': {'keyname': 'KEYPAD_TWO', 'keycode': '90'},
-          'W': {'keyname': 'KEYPAD_FORWARD_SLASH', 'keycode': '84'},
-          'X': {'keyname': 'RIGHT_ARROW', 'keycode': '79'}},
-    'N': {'Q': {'keyname': 'KEYPAD_PERIOD', 'keycode': '99'},
-          'S': {'keyname': 'PAGE_UP', 'keycode': '75'},
-          'T': {'keyname': 'PAGE_DOWN', 'keycode': '78'},
-          'U': {'keyname': 'KEYPAD_NINE', 'keycode': '97'},
-          'V': {'keyname': 'KEYPAD_THREE', 'keycode': '91'},
-          'W': {'keyname': 'KEYPAD_ASTERISK', 'keycode': '85'},
-          'X': {'keyname': 'KEYPAD_MINUS', 'keycode': '86'}},
-    'O': {'Q': {'keyname': 'UP_ARROW', 'keycode': '82'},
-          'S': {'keyname': 'HOME', 'keycode': '74'},
-          'T': {'keyname': 'END', 'keycode': '77'},
-          'U': {'keyname': 'KEYPAD_PLUS', 'keycode': '87'},
-          'V': {'keyname': 'KEYPAD_ENTER', 'keycode': '96'},
-          'W': {'keyname': 'PAUSE', 'keycode': '72'},
-          'X': {'keyname': 'LEFT_ARROW', 'keycode': '80'}},
-    'P': {'Q': {'keyname': 'LEFT_ALT', 'keycode': '226'},
-          'T': {'keyname': 'PRINT_SCREEN', 'keycode': '70'},
-          'U': {'keyname': 'SCROLL_LOCK', 'keycode': '71'},
-          'X': {'keyname': 'RIGHT_ALT', 'keycode': '230'}}
+ 'A': {'S': {'keyname': 'LEFT_CONTROL', 'keycode': '224'},
+       'W': {'keyname': 'RIGHT_CONTROL', 'keycode': '228'}},
+ 'B': {'W': {'keyname': 'RIGHT_SHIFT', 'keycode': '229'}},
+ 'C': {'Q': {'keyname': 'ESCAPE', 'keycode': '41'},
+       'S': {'keyname': 'GRAVE_ACCENT', 'keycode': '53'},
+       'T': {'keyname': 'ONE', 'keycode': '30'},
+       'U': {'keyname': 'Q', 'keycode': '20'},
+       'V': {'keyname': 'A', 'keycode': '4'},
+       'W': {'keyname': 'Z', 'keycode': '29'}},
+ 'D': {'S': {'keyname': 'F1', 'keycode': '58'},
+       'T': {'keyname': 'TWO', 'keycode': '31'},
+       'U': {'keyname': 'W', 'keycode': '26'},
+       'V': {'keyname': 'S', 'keycode': '22'},
+       'W': {'keyname': 'X', 'keycode': '27'}},
+ 'E': {'Q': {'keyname': 'F4', 'keycode': '61'},
+       'S': {'keyname': 'F2', 'keycode': '59'},
+       'T': {'keyname': 'THREE', 'keycode': '32'},
+       'U': {'keyname': 'E', 'keycode': '8'},
+       'V': {'keyname': 'D', 'keycode': '7'},
+       'W': {'keyname': 'C', 'keycode': '6'}},
+ 'F': {'Q': {'keyname': 'G', 'keycode': '10'},
+       'S': {'keyname': 'FIVE', 'keycode': '34'},
+       'T': {'keyname': 'FOUR', 'keycode': '33'},
+       'U': {'keyname': 'R', 'keycode': '21'},
+       'V': {'keyname': 'F', 'keycode': '9'},
+       'W': {'keyname': 'V', 'keycode': '25'},
+       'X': {'keyname': 'B', 'keycode': '5'}},
+ 'G': {'Q': {'keyname': 'F5', 'keycode': '62'},
+       'S': {'keyname': 'F9', 'keycode': '66'},
+       'T': {'keyname': 'F10', 'keycode': '67'},
+       'V': {'keyname': 'BACKSLASH', 'keycode': '49'},
+       'W': {'keyname': 'ENTER', 'keycode': '40'},
+       'X': {'keyname': 'SPACE', 'keycode': '44'}},
+ 'H': {'Q': {'keyname': 'H', 'keycode': '11'},
+       'S': {'keyname': 'SIX', 'keycode': '35'},
+       'T': {'keyname': 'SEVEN', 'keycode': '36'},
+       'U': {'keyname': 'U', 'keycode': '24'},
+       'V': {'keyname': 'J', 'keycode': '13'},
+       'W': {'keyname': 'M', 'keycode': '16'},
+       'X': {'keyname': 'N', 'keycode': '17'}},
+ 'I': {'Q': {'keyname': 'F6', 'keycode': '63'},
+       'S': {'keyname': 'EQUALS', 'keycode': '46'},
+       'T': {'keyname': 'EIGHT', 'keycode': '37'},
+       'U': {'keyname': 'I', 'keycode': '12'},
+       'V': {'keyname': 'K', 'keycode': '14'},
+       'W': {'keyname': 'COMMA', 'keycode': '54'}},
+ 'J': {'S': {'keyname': 'F8', 'keycode': '65'},
+       'T': {'keyname': 'NINE', 'keycode': '38'},
+       'U': {'keyname': 'O', 'keycode': '18'},
+       'V': {'keyname': 'L', 'keycode': '15'},
+       'W': {'keyname': 'PERIOD', 'keycode': '55'}},
+ 'K': {'Q': {'keyname': 'QUOTE', 'keycode': '52'},
+       'S': {'keyname': 'MINUS', 'keycode': '45'},
+       'T': {'keyname': 'ZERO', 'keycode': '39'},
+       'U': {'keyname': 'P', 'keycode': '19'},
+       'V': {'keyname': 'SEMICOLON', 'keycode': '51'},
+       'X': {'keyname': 'FORWARD_SLASH', 'keycode': '56'}},
+ 'L': {'S': {'keyname': 'DELETE', 'keycode': '76'},
+       'T': {'keyname': 'F11', 'keycode': '68'},
+       'U': {'keyname': 'KEYPAD_SEVEN', 'keycode': '95'},
+       'V': {'keyname': 'KEYPAD_ONE', 'keycode': '89'},
+       'W': {'keyname': 'KEYPAD_NUMLOCK', 'keycode': '83'},
+       'X': {'keyname': 'DOWN_ARROW', 'keycode': '81'}},
+ 'M': {'Q': {'keyname': 'KEYPAD_ZERO', 'keycode': '98'},
+       'S': {'keyname': 'INSERT', 'keycode': '73'},
+       'T': {'keyname': 'F12', 'keycode': '69'},
+       'U': {'keyname': 'KEYPAD_EIGHT', 'keycode': '96'},
+       'V': {'keyname': 'KEYPAD_TWO', 'keycode': '90'},
+       'W': {'keyname': 'KEYPAD_FORWARD_SLASH', 'keycode': '84'},
+       'X': {'keyname': 'RIGHT_ARROW', 'keycode': '79'}},
+ 'N': {'Q': {'keyname': 'KEYPAD_PERIOD', 'keycode': '99'},
+       'S': {'keyname': 'PAGE_UP', 'keycode': '75'},
+       'T': {'keyname': 'PAGE_DOWN', 'keycode': '78'},
+       'U': {'keyname': 'KEYPAD_NINE', 'keycode': '97'},
+       'V': {'keyname': 'KEYPAD_THREE', 'keycode': '91'},
+       'W': {'keyname': 'KEYPAD_ASTERISK', 'keycode': '85'},
+       'X': {'keyname': 'KEYPAD_MINUS', 'keycode': '86'}},
+ 'O': {'Q': {'keyname': 'UP_ARROW', 'keycode': '82'},
+       'S': {'keyname': 'HOME', 'keycode': '74'},
+       'T': {'keyname': 'END', 'keycode': '77'},
+       'U': {'keyname': 'KEYPAD_PLUS', 'keycode': '87'},
+       'V': {'keyname': 'KEYPAD_ENTER', 'keycode': '96'},
+       'W': {'keyname': 'PAUSE', 'keycode': '72'},
+       'X': {'keyname': 'LEFT_ARROW', 'keycode': '80'}},
+ 'P': {'Q': {'keyname': 'LEFT_ALT', 'keycode': '226'},
+       'T': {'keyname': 'PRINT_SCREEN', 'keycode': '70'},
+       'U': {'keyname': 'SCROLL_LOCK', 'keycode': '71'},
+       'X': {'keyname': 'RIGHT_ALT', 'keycode': '230'}}
 }
 
 
@@ -131,20 +132,22 @@ V = {'pin': digitalio.DigitalInOut(board.GP21), 'name': 'V'}
 W = {'pin': digitalio.DigitalInOut(board.GP22), 'name': 'W'}
 X = {'pin': digitalio.DigitalInOut(board.GP26), 'name': 'X'}
 
+# I mainly setup everything this way so it'd be easy to swap the matrix if things got weird
+cols = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P]
+rows = [Q,R,S,T,U,V,W,X]
 
-cols = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P]
-rows = [Q, R, S, T, U, V, W, X]
 
 # Set all columns as outputs
 for col in cols:
     col['pin'].switch_to_output()
 
-# Set all rows as inputs and set the pulldown resistors
+# Set all rows as inputs and set the internal pulldown resistors (this way, a keypress registers as HIGH; or True for CircuitPython)
 for row in rows:
     row['pin'].switch_to_input(pull=digitalio.Pull.DOWN)
 
 
 # Initialize the dictionary we will use to track pin states and info
+# This is mainly used for debouncing
 pin_dict = {}
 for col in cols:
     pin_dict[col['name']] = {}
@@ -163,13 +166,13 @@ for col in cols:
 
 # Initialize a dictionary with all of the modifier keys so we can check them separately later
 modifier_dict = {}
-for k, v in key_map.items():
-    for k2, v2 in v.items():
+for k,v in key_map.items():
+    for k2,v2 in v.items():
         if 'SHIFT' in v2['keyname'] or 'ALT' in v2['keyname'] or 'CONTROL' in v2['keyname']:
-            modifier_dict[v2['keyname']] = {
-                'col': k, 'row': k2, 'keycode': v2['keycode']}
+            modifier_dict[v2['keyname']] = {'col': k, 'row': k2, 'keycode': v2['keycode']}
 
 
+# Returns the digitalio pin object that will let us see the current value
 def getPin(matrix_letter):
     for col in cols:
         if matrix_letter == col['name']:
@@ -179,8 +182,9 @@ def getPin(matrix_letter):
             return row['pin']
 
 
+# Checks all the modifier keys and presses them if true
 def checkAndPressModifiers(is_shift, is_alt, is_ctrl):
-    for k, v in modifier_dict.items():
+    for k,v in modifier_dict.items():
         column_pin = getPin(v['col'])
         row_pin = getPin(v['row'])
 
@@ -199,22 +203,30 @@ def checkAndPressModifiers(is_shift, is_alt, is_ctrl):
     return is_shift, is_alt, is_ctrl
 
 
-debounceDelay = 0
-holdTimeCheck = 180
-is_modifier = False
 
+
+debounceDelay = 0 # I have it set to 0 and that seems to be fine. But other setups may require a higher delay
+holdTimeCheck = 180 # This is basically how long (in ms) you need to hold a key before it will start spamming that key
+
+is_modifier = False
 is_shift = False
 is_alt = False
 is_ctrl = False
 
 loop = True
 while loop:
-    is_shift, is_alt, is_ctrl = checkAndPressModifiers(
-        is_shift, is_alt, is_ctrl)
+    # First, check all the modifier keys and see if we're pressing any. This makes the modifying logic a lot easier
+    is_shift, is_alt, is_ctrl = checkAndPressModifiers(is_shift, is_alt, is_ctrl)
+    
+    # Loop through all the columns so we can start scanning
     for col in cols:
+        # Send power down the column so we can begin reading keys row by row
+        # Here, we're hunting for which key in the column (if any) is being pressed
         col['pin'].value = True
         for row in rows:
             pin = pin_dict[col['name']][row['name']]
+            
+            # These next few lines will be removed once I have all the keys mapped. I have some broken keys and they aren't in the key_map
             keyname = 'wee'
             try:
                 keyname = key_map[col['name']][row['name']]['keyname']
@@ -224,6 +236,7 @@ while loop:
             # Set if it's a modifier so we can skip them. Stuff gets super weird if we don't
             is_modifier = 'SHIFT' in keyname or 'ALT' in keyname or 'CONTROL' in keyname
 
+            # Set the current pin state so we can use it later for debouncing
             pin['current_pin_state'] = row['pin'].value
 
             current_time = adafruit_ticks.ticks_ms()
@@ -232,25 +245,28 @@ while loop:
             if currentBtnState != pin['last_pin_state']:
                 pin['last_debounce_time'] = current_time
 
+            # If the time between when we first registered the key being pressed and now is greater than the debounce delay, set the actual state to True or "pressed"
             if (current_time - pin['last_debounce_time']) > debounceDelay:
                 pin['actual_state'] = currentBtnState
 
+            # If the key being pressed is not a modifier key and we're going from not-pressed to pressed and we're clear of the debounce delay, press the key
             if not is_modifier and (pin['last_actual_state'] == 0) and (pin['actual_state'] == 1):
                 kbd.press(int(key_map[col['name']][row['name']]['keycode']))
-                # print('pressed', col['name'], row['name'], key_map[col['name']][row['name']]['keyname'])
+                #print('pressed', col['name'], row['name'], key_map[col['name']][row['name']]['keyname'])
+            # Otherwise, if the key is not a modifier key and we're currently holding the key, start a hold time check
             elif not is_modifier and (pin['last_actual_state'] == 1) and (pin['actual_state'] == 1):
                 if pin['start_hold_time'] == 0:
                     pin['start_hold_time'] = current_time
+                # If we're actually holding the key, and we've cleared the hold time check, press the key
                 if pin['start_hold_time'] > 0 and (current_time - pin['start_hold_time']) > holdTimeCheck:
                     pin['holding_state'] = True
-                    kbd.press(int(key_map[col['name']]
-                              [row['name']]['keycode']))
-                    # print('holding', col['name'], row['name'], key_map[col['name']][row['name']]['keyname'])
+                    kbd.press(int(key_map[col['name']][row['name']]['keycode']))
+                    #print('holding', col['name'], row['name'], key_map[col['name']][row['name']]['keyname'])
+            # Otherwise, if we're going from pressed to not-pressed, release the key
             elif (pin['last_actual_state'] == 1) and (pin['actual_state'] == 0):
-                keycode = int(key_map[col['name']][row['name']]['keycode'])
                 pin['start_hold_time'] = 0
                 pin['holding_state'] = False
-                kbd.release(keycode)
+                kbd.release(int(key_map[col['name']][row['name']]['keycode']))
                 if is_modifier:
                     if is_shift:
                         is_shift = False
@@ -261,7 +277,17 @@ while loop:
                     elif is_ctrl:
                         is_ctrl = False
                         kbd.release(Keycode.CONTROL)
-                # print('released', col['name'], row['name'], key_map[col['name']][row['name']]['keyname'])
+                #print('released', col['name'], row['name'], key_map[col['name']][row['name']]['keyname'])
             pin['last_actual_state'] = pin['actual_state']
             pin['last_pin_state'] = pin['current_pin_state']
         col['pin'].value = False
+
+
+
+
+
+
+
+
+
+
